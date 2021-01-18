3016abdbe6b27d8c69164803a5abcf7e5e7e6152
Anton Vishnevski <oracle_X@hotmail.com>
2021-01-18 18:13:13 +0200

    Tests passed

--- neuron.py
+++ neuron.py
@@ -10,17 +10,17 @@ class Neuron:
 
 	# Initialization.
 	def __init__(self, initialize_dendrites):
-		self.initialize_dendrites = initialize_dendrites
 		self.dendrites = {}
 		self.activation_matrix = []
+		self.power_matrix = []
 		self.nucleus_threshold = 0.8
 		self.nucleus = 0
 		# Dendrites base activation.
 		x = 0
-		while x < self.initialize_dendrites:
-			x += 1
+		while x < initialize_dendrites:
 			self.dendrites["dendrite_" + str(x)] = 0
 			self.activation_matrix.append(0)
+			x += 1
 	
 	# Assume, the power to activate a neuron could be positive or negative.
 	# Initial state is random
@@ -32,13 +32,27 @@ class Neuron:
 	# Cogitation process, compare activation vs mean sum of dendrites.
 	def cogitate(self, *activation_matrix):
 		"""Cogitation process"""
-		# Defining variables.
-		dendrites_sum = sum(self.dendrites.values())
-		dendrites_mean = dendrites_sum / self.initialize_dendrites
+		# Checking new activation parameters.
+		if activation_matrix:
+			self.activation_matrix = activation_matrix
+
+		# Building power matrix out of activation matrix and dendrites power.
+		for x in range(0, len(self.activation_matrix)):
+			if self.activation_matrix[x] == 1:
+				dendrite_power = self.dendrites["dendrite_" + str(x)]
+				self.power_matrix.append(dendrite_power)
+
+		# Generating average from power matrix.
+		power_matrix = self.power_matrix
+		try:
+			power_matrix_average = sum(power_matrix) / len(power_matrix)
+		except ZeroDivisionError:
+		# If there are no active dendrites, avoid 0 division, by giving 0.
+			power_matrix_average = 0
 		nucleus_threshold = self.nucleus_threshold
 
 		# Checking activation.
-		if dendrites_mean >= nucleus_threshold:
+		if power_matrix_average >= nucleus_threshold:
 			self.nucleus = 1
 		else:
 			self.nucleus = 0
